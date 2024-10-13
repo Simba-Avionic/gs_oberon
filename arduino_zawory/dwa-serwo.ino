c35ef0e383bd8c7a310226933fac7dabf07b7a41
janlandecki <105974541+janlandecki@users.noreply.github.com>
2024-10-13 17:52:34 +0200

    Update dwa-serwo.ino

--- arduino_zawory/dwa-serwo.ino
+++ arduino_zawory/dwa-serwo.ino
@@ -1,86 +1,63 @@
 #include <Servo.h>
+#define GSUART_PLATFORM_ARDUINO     0
+#define GSUART_PLATFORM_RPI_UBUNTU  1
+#define GSUART_PLATFORM GSUART_PLATFORM_ARDUINO
+#include "GSUART.hpp"
+GSUART::Messenger messenger(&Serial);
+#define SEND_INTERVAL_UART_STATS  4000
+#define FEED_PIN 9
+#define VENT_PIN 10
+#define RESET_TIME 5000
 
-Servo serwo1;
-Servo serwo2;
-char pos1 = 0, pos2 = 0;
+void sendUartStats();
+Servo serwo_vent;
+Servo serwo_feed;
+int8_t pos_vent = 0, pos_feed = 0;
 unsigned long last_update = 0;
-unsigned long time = 0;
-void ramka_in();
 
 void setup() {
-  serwo1.attach(9);
-  serwo2.attach(10);
+  serwo_feed.attach(FEED_PIN);
+  serwo_vent.attach(VENT_PIN);
   Serial.begin(9600);
 }
 
 void loop() {
-if (Serial.available())
-  ramka_read();
-int value = map(static_cast<int>(pos1), 0, 100, 0, 180);
-serwo1.write(value);
-value = map(static_cast<int>(pos2), 0, 100, 0, 180);
-serwo2.write(value);
-
-time = millis();
-if (time - last_update > 5000){
-  serwo1.write(0);
-  serwo2.write(0);
+static unsigned long uart_stats_read_time = millis();
+unsigned long tic = millis();
+if (millis() >= uart_stats_read_time)
+  {
+    uart_stats_read_time = millis() + SEND_INTERVAL_UART_STATS;
+    sendUartStats();
+  } 
+readServoCtrl();
+if (tic - last_update >= RESET_TIME){
+  pos_vent = 0;
+  pos_feed = 0;
   last_update = millis();
 }
+int value = map(static_cast<int>(pos_vent), 0, 100, 0, 180);
+serwo_vent.write(value);
+value = map(static_cast<int>(pos_feed), 0, 100, 0, 180);
+serwo_feed.write(value);
 }
 
-void ramka_read(){
-unsigned char ramka[128];
-unsigned int ramka_size = 0;
-bool special = false;
-if (Serial.read() == 0x03){
-  while (true){
-    unsigned char buf = Serial.read();
-    if (!special){
-      if (buf == 0x33){
-        ramka_decode(ramka, ramka_size);
-        return;
-      }
-      else if (buf == 0x03){  // bajt startu resetuje ramkę 
-        for (int i = 0; i < ramka_size; ++i){
-          ramka[i] = NULL;
-        }
-        ramka_size = 0;
-        continue; // to moze nie dzialac
-      }
-    }
-    else
-      special = false;
-    if (buf == 0xF0)
-      special = true;
-    if (ramka_size > 127)
-      return;
-    ramka[ramka_size] = buf;
-    ++ramka_size;
+void readServoCtrl(){
+  GSUART::Message* msg = nullptr;
+  msg = messenger.receive();
+
+  if (msg == nullptr)
+    return;
+  switch (msg->getID()){
+    case GSUART::MsgID::ZAWORY_STEROWANIE:
+       GSUART::MsgZaworySterowanie* msgZaworyS = dynamic_cast<GSUART::MsgZaworySterowanie*>(msg);
+       pos_vent = msgZaworyS->valve_vent;
+       pos_feed = msgZaworyS->valve_feed;
+       last_update = millis();
+    default:
+      break;
   }
 }
-}
-
-void ramka_decode(unsigned char* ramka, unsigned int size){  //do funkcji wysyłamy ramkę bez bajtu startu i stopu, bo po co
-unsigned char ramka_r[128];
-unsigned int id_r = 0;
-for (unsigned int i = 0; i<size; ++i){
-  if (ramka[i] == 0xF0)
-    ramka[++i] += 0x20;
-  ramka_r[id_r] = ramka[i];
-  ++id_r;
-}
-unsigned char checksuma = 0;
-for (unsigned int i = 0; i<id_r-1; ++i){
-  checksuma += ramka_r[i];
-}
-if (checksuma != ramka_r[id_r - 1])
-  return;
-if (ramka_r[0] == 0x03){
-  if (ramka_r[4] != NULL) //warunek do odrzucania zbyt długich ramek
-    return;
-  pos1 = static_cast<char>(ramka_r[1]);
-  pos2 = static_cast<char>(ramka_r[2]);
-  last_update = millis();
-}
+void sendUartStats()
+{
+    messenger.sendUartStats();
 }
